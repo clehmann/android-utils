b77a55700585a7aa4fdc5f00640f8bc4fdf69f4f
Chris Lehmann <lehmannc.spam@gmail.com>
2011-06-26 12:11:47 -0500

    * Working on museum downlads

--- src/main/java/net/chrislehmann/common/util/FileSystemUtils.java
+++ src/main/java/net/chrislehmann/common/util/FileSystemUtils.java
@@ -2,6 +2,10 @@ package net.chrislehmann.common.util;
 
 import android.os.Environment;
 import android.os.StatFs;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class FileSystemUtils {
@@ -12,6 +16,7 @@ public class FileSystemUtils {
         double sdAvailSize = (double) stat.getAvailableBlocks() * (double) stat.getBlockSize();
         return sdAvailSize / BYTES_IN_GB;
     }
+
     public static double getTotalSpaceInGb() {
         StatFs stat = new StatFs(Environment.getExternalStorageDirectory().getPath());
         double sdTotalSize = (double) stat.getBlockCount() * (double) stat.getBlockSize();
@@ -22,4 +27,18 @@ public class FileSystemUtils {
         return getTotalSpaceInGb() - getFreeSpaceInGb();
     }
 
+
+    public static boolean doesFileExist(String path){
+        return new File(path).exists();
+    }
+
+    public static void forceDeleteFile(String path) {
+        if(doesFileExist(path))
+        try {
+            FileUtils.forceDelete(new File(path));
+        } catch (IOException e) {
+            throw new RuntimeException("Error deleting file: " + path, e);
+        }
+
+    }
 }
