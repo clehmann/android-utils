582f4d20bb37575d42cea4189dccad8f2c3df8fb
Chris Lehmann <lehmannc.spam@gmail.com>
2011-07-13 11:25:19 -0500

    * Use android-utils for PreferencesUtility * Check for in-progress call on CallActivity start

--- src/main/java/net/chrislehmann/common/util/PreferencesUtilities.java
+++ src/main/java/net/chrislehmann/common/util/PreferencesUtilities.java
@@ -4,25 +4,51 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-/**
- * Created by IntelliJ IDEA.
- * User: clehmann
- * Date: 5/26/11
- * Time: 7:59 PM
- * To change this template use File | Settings | File Templates.
- */
+
 public class PreferencesUtilities {
 
 
-    public static String getPrefereceAsString(Context context, int preferenceId, String defaultValue) {
+    public static String getPreferenceAsString(Context context, int preferenceId, String defaultValue) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         return preferences.getString(context.getText(preferenceId).toString(), defaultValue);
     }
 
-    public static int getPrefereceAsIntRefactored(Context context, int preferenceId, int defaultValue) {
+    public static int getPreferenceAsInt(Context context, int preferenceId, int defaultValue) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         return preferences.getInt(context.getText(preferenceId).toString(), defaultValue);
     }
 
+    public static boolean getPreferenceAsBoolean(Context context, int preferenceId, boolean defaultValue) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getBoolean(context.getText(preferenceId).toString(), defaultValue);
+    }
+
+    public static void savePreference(Context context, int preferenceId, String value) {
+        SharedPreferences.Editor editor = getEditor(context);
+        editor.putString(context.getString(preferenceId), value);
+        editor.commit();
+    }
+    
+    public static void savePreference(Context context, int preferenceId, boolean value) {
+        SharedPreferences.Editor editor = getEditor(context);
+        editor.putBoolean(context.getString(preferenceId), value);
+        editor.commit();
+    }
+
+    public static void savePreference(Context context, int preferenceId, int value) {
+        SharedPreferences.Editor editor = getEditor(context);
+        editor.putInt(context.getString(preferenceId), value);
+        editor.commit();
+    }
+
+    private static SharedPreferences.Editor getEditor(Context context) {
+        final SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return defaultSharedPreferences.edit();
+    }
+
+    public static boolean isPreferenceSet(Context context, int preferenceId) {
+        final SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return defaultSharedPreferences.contains(context.getString(preferenceId));
+    }
 
 }
